041eb3320f53af811899f9875c8b7cb87128b99e
joelnicolass <joelnicolass@gmail.com>
2025-05-02 02:19:51 -0300

    feat: correcciones menores

--- src/scenes/unit/unit.gd
+++ src/scenes/unit/unit.gd
@@ -4,12 +4,7 @@ class_name Unit extends StaticBody3D
 var initial_transform: Transform3D
 var peer: int
 
-func _enter_tree() -> void:
-	set_multiplayer_authority(peer)
-	print("Unit " + str(name) + "peer:" + str(peer) + " has authority: " + str(is_multiplayer_authority()))
-
-
-@export var movement_speed: float = 4.0
+@export var movement_speed: float = 30.0
 @onready var selected = $Selected
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 @onready var map_RID: RID = get_world_3d().get_navigation_map()
@@ -20,11 +15,18 @@ var pathing_point: int = 0
 var path_points_packed: PackedVector3Array
 
 
-func unit_path_new(wanted_goal: Vector3) -> void:
-	var safe_goal: Vector3 = NavigationServer3D.map_get_closest_point(map_RID, wanted_goal)
-	path_points_packed = NavigationServer3D.map_get_path(map_RID, global_position, safe_goal, true)
-	pathing = true
-	pathing_point = 0
+func _enter_tree() -> void:
+	set_multiplayer_authority(peer)
+	print("Unit " + str(name) + "peer:" + str(peer) + " has authority: " + str(is_multiplayer_authority()))
+
+
+func _ready() -> void:
+	if not is_multiplayer_authority(): return
+
+	global_transform = initial_transform
+
+	await (get_tree().process_frame)
+	_y_correction()
 
 
 func _physics_process(delta: float) -> void:
@@ -34,7 +36,7 @@ func _physics_process(delta: float) -> void:
 		var path_next_point: Vector3 = path_points_packed[pathing_point] - global_position
 		if path_next_point.length_squared() > 1.0:
 			var velocity: Vector3 = (path_next_point.normalized() * delta) * movement_speed
-			unit_rotate_to_direction(velocity)
+			_unit_rotate_to_direction(velocity)
 			global_position += velocity
 		else:
 			if pathing_point < (path_points_packed.size() - 1):
@@ -59,18 +61,26 @@ func move_to(movement_target: Vector3) -> void:
 		pathing = false
 		pathing_point = 0
 
-	unit_path_new(movement_target)
+	_unit_path_new(_random_position_offset(movement_target))
+
+
+func _unit_rotate_to_direction(direction: Vector3) -> void:
+	rotation.y = atan2(-direction.x, -direction.z)
 	
 
-func _ready() -> void:
-	if not is_multiplayer_authority(): return
+func _unit_path_new(wanted_goal: Vector3) -> void:
+	var safe_goal: Vector3 = NavigationServer3D.map_get_closest_point(map_RID, wanted_goal)
+	path_points_packed = NavigationServer3D.map_get_path(map_RID, global_position, safe_goal, true)
+	pathing = true
+	pathing_point = 0
 
-	global_transform = initial_transform
-	
-	await (get_tree().process_frame)
+
+func _y_correction() -> void:
 	global_position = NavigationServer3D.map_get_closest_point(map_RID, global_position)
 	mesh.position.y = NavigationServer3D.map_get_cell_height(map_RID)
 
 
-func unit_rotate_to_direction(direction: Vector3) -> void:
-	rotation.y = atan2(-direction.x, -direction.z)
+func _random_position_offset(target_direction: Vector3) -> Vector3:
+	var randon_factor: float = 4.0
+	var offset = Vector3(randf_range(-randon_factor, randon_factor), 0, randf_range(-randon_factor, randon_factor))
+	return target_direction + offset
